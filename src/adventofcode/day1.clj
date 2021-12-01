d0e018ea5766e1f5931bff68d99ddede0439fa64
barthr <info@bartfokker.com>
2021-12-01 22:44:37 +0100

    Add second challenge!

--- src/adventofcode/day1.clj
+++ src/adventofcode/day1.clj
@@ -11,3 +11,18 @@
 
 (defn read-input []
   (->> "resources/day1.txt" slurp str/split-lines (map #(Integer/parseInt %))))
+
+; (max-sequence (read-input))
+
+; Second part
+
+(defn sum [x] (reduce #(+ %) 0 x))
+
+(defn max-sequence-of-subsequent-partitions [input]
+  (reduce
+    (fn [res [current, next]]
+      (+ res (if (> (reduce + 0 next) (reduce + 0 current)) 1 0)))
+    0
+    (partition 2 1 input)))
+
+; (max-sequence-of-subsequent-partitions (partition 3 (read-input)))
